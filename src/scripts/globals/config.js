688dd7360805b7d446fa31402afc1c3bd7f86b05
MuhAsmann <muhasmangit@gmail.com>
2023-03-25 16:42:57 +0700

    Initialize | Belum Selesai

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  KEY: '8556ef86c62d78bd9779cee48c94bcec',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+};
+ 
+export default CONFIG;
